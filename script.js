55a5b7b2eb4cc2dc6860481071f9a05f214511b5
Karabo Rithuri <rithurikarabo@gmail.com>
2023-10-07 14:41:15 +0200

    code to run if cartItems != 0

--- script.js
+++ script.js
@@ -54,5 +54,19 @@ function displayCart(a){
     let j=0;
     if(categories.length == 0){
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
+    }else{
+        document.getElementById('cartItem').innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            return(
+                `<div class='cart-item>
+                <div class='row-img>
+                    <img class='rowing' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size:15px;'>${price}</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++)+ ")'></i></div>"
+            );
+        }).join('')
     }
 }
